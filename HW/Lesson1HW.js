f57b64855485bc39435951e47cbba80e5d6aa68b
Exelleron <exelleron1@gmail.com>
2022-01-07 18:19:58 +0200

    'task2'

---
+++ HW/Lesson1HW.js
@@ -0,0 +1,13 @@
+console.log('Task 2');
+
+console.log(typeof 9);
+console.log(typeof NaN);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof []);
+console.log(typeof {});
+console.log(typeof function () {});
+console.log(typeof 'string');
+console.log(typeof 11n);
+console.log(typeof true);
+console.log(typeof Symbol('ip'));
