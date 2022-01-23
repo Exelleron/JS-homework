21628bf87e56ffc5d0d6531a43a73c5200a0fc34
Exelleron <exelleron1@gmail.com>
2022-01-24 00:28:42 +0200

    'Task1'

--- HW/lesson5HW.js
+++ HW/lesson5HW.js
@@ -1,10 +1,11 @@
-const studentArr = [{
-    name: 'Сергей',
-    surname: 'Войлов',
-    ratingPoint: 1000,
-    schoolPoint: 1000,
-    course: 2,
-},
+const studentArr = [
+    {
+        name: 'Сергей',
+        surname: 'Войлов',
+        ratingPoint: 1000,
+        schoolPoint: 1000,
+        course: 2,
+    },
     {
         name: 'Татьяна',
         surname: 'Коваленко',
@@ -78,23 +79,84 @@ const studentArr = [{
 ];
 
 
+console.log('Task 1');
 
+class Student {
+    constructor(enrollee = {}) {
+        this.id = Student.incrementId();
+        this.name = enrollee.name;
+        this.surname = enrollee.surname;
+        this.ratingPoint = enrollee.ratingPoint;
+        this.schoolPoint = enrollee.schoolPoint;
+        Student.listOfStudents = {id: this.id, ...enrollee,};
+    }
+
+    static get listOfStudents() {
+        return this.studentsList || []
+    }
+
+    static set listOfStudents(student) {
+        const studentList = this.studentsList || [];
+        student.isSelfPayment = true;
+        if (student.ratingPoint >= 800 && studentList.length < 5) {
+            student.isSelfPayment = false;
+        }
+        if (studentList.length >= 5) {
+            let notSelfPayment = studentList.filter((person) => !person.isSelfPayment);
+            let selfPayment = studentList.filter((person) => person.isSelfPayment);
+
+            if (student.ratingPoint >= 800) {
+                student.isSelfPayment = false;
+                notSelfPayment.unshift(student);
+
+                if (notSelfPayment.length > 5) {
+                    let notSelfPaymentSort = notSelfPayment.sort((a, b) => a.ratingPoint - b.ratingPoint);
+                    if (notSelfPaymentSort[0].ratingPoint === notSelfPaymentSort[1].ratingPoint) {
+                        notSelfPaymentSort[0].isSelfPayment = notSelfPaymentSort[0].schoolPoint < notSelfPaymentSort[1].schoolPoint;
+                        notSelfPaymentSort[1].isSelfPayment = notSelfPaymentSort[0].schoolPoint > notSelfPaymentSort[1].schoolPoint;
+                    } else {
+                        notSelfPaymentSort[0].isSelfPayment = notSelfPaymentSort[0].ratingPoint < notSelfPaymentSort[1].ratingPoint;
+                        notSelfPaymentSort[1].isSelfPayment = notSelfPaymentSort[0].ratingPoint > notSelfPaymentSort[1].ratingPoint;
+                    }
+                    notSelfPayment = notSelfPaymentSort;
+                }
+            } else {
+                selfPayment.push(student)
+            }
+            return this.studentsList = [...notSelfPayment, ...selfPayment];
+        }
+        return this.studentsList = [...studentList, student];
+    }
+
+    static incrementId() {
+        if (!this.latestId) this.latestId = 1;
+        else this.latestId++;
+        return this.latestId;
+    }
+}
+
+studentArr.forEach(student => new Student(student));
+console.log(Student.listOfStudents);
+
+// console.log(new Student(studentArr));
+// console.log(new Student(studentArr[1]));
+// console.log(new Student(studentArr[2]));
+// console.log(new Student(studentArr[3]));
 
 ///////////////////////////
 
 console.log('Task 2');
 
 class CustomString {
-    constructor(){
-
-    }
-    reverse(string){
+    reverse(string) {
         return string.split('').reverse().join('');
     }
-    ucFirst(string){
+
+    ucFirst(string) {
         return string = string[0].toUpperCase() + string.slice(1);
     }
-    ucWords(string){
+
+    ucWords(string) {
         return string = string.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
     }
 }
